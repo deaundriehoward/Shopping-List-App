bd5dca26ce69738d46758969a5b565e70b793d39
De'Aundrie Howard <deaundrie7@gmail.com>
2015-09-26 09:33:16 -0500

    Initial commit

--- app.js
+++ app.js
@@ -3,18 +3,20 @@ $(document).ready(function() {
 	// When the user click the "Add Item" button
 	$('.add-button').on('click', function() {
 			var newItem = $('input.add-item').val();
-
 			// Alert: When the user enters less than required 
 			if (newItem == 0) {
 			alert("Enter an item. Please and thank you!");
-		}
+		} 
 			// When the user enter a new item
-			$('ul').append('<li class="buy"><input class="item" type="checkbox"></input' + newItem + '</li>');
+			var newNote = $('input.note').val();
+			var newCat = $('select.categories').val();
+
+			$('ol').append('<li class="buy"><button class="item"> Got it! </button>' + newItem + newNote + newCat + '</li>');
 	});
 
 		// When the user wants to remove or have bought an item
-		$('ul').on('click', '.item', function() {
+		$('ol').on('click', '.item', function() {
 		$(this).closest('li').toggleClass('buy bought');
-		$(this).remove();
+		$(this).remove('input.quantity');
 	});
 });
